172449c0907e6d7d1734a6069a5db16d21c10240
Kritansh Tank <tankkritansh088@gmail.com>
2024-11-07 16:06:44 +0530

    Update page.jsx

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -6,7 +6,7 @@ import { useRef } from "react";
 import img1 from "../../../public/1.png";
 import img2 from "../../../public/2.png";
 import img3 from "../../../public/8.png";
-import img4 from "../../../public/4.png";
+import img4 from "../../../public/9.png";
 import img5 from "../../../public/5.png";
 import img6 from "../../../public/6.png";
 import img7 from "../../../public/7.png";
