2506c930af447c9d3317e7149cf35cda20b8ee88
Kritansh Tank <tankkritansh088@gmail.com>
2025-03-13 14:08:00 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,7 +43,7 @@ const Homepage = () => {
               Contact Me
               </button>
             </Link>
-            <Link href="https://drive.google.com/file/d/156arCDFJD1ZOXs6RTQVaaeRfWAlFzAMD/view?usp=drive_link">
+            <Link href="https://drive.google.com/file/d/1ihgCQpropXdWnthGtTIQLwWGgbLsEobn/view?usp=drive_link">
               <button className="p-3.5 rounded-lg bg-white text-black">
                 Resume
               </button>
