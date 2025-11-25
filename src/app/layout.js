b70143282eb7d02036d36505d5b5c77e1e8bd48a
Kritansh Tank <tankkritansh088@gmail.com>
2025-11-25 15:02:21 +0530

    Update metadata title and description

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import TransitionProvider from "@/components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Kritansh Tank Portfolio App",
-  description: "The best animated portfolio page",
+  title: "Kritansh Tank",
+  description: "Portfolio Website",
 };
 
 export default function RootLayout({ children }) {
